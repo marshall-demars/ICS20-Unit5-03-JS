467e0bfd32142b51aa20cef40932a73030e4f1c0
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-04-28 16:50:37 +0000

    6 files

--- js/script.js
+++ js/script.js
@@ -8,14 +8,28 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS20-Unit5-03-JS/sw.js", {
+    scope: "/ICS20-Unit5-03-JS/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function calculates the cost of a pizza
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
-}
+  // input
+  const adults = document.getElementById("adult-age").checked
+  const teenagers = document.getElementById("teen-age").checked
+  const kids = document.getElementById("kid-age").checked
+  const babies = document.getElementById("baby-age").checked
+  
+  // process and output
+  if (adults == true) {
+    console.log("You can watch an R rated movie alone!")
+  } else if (teenagers == true) {
+    console.log("You can watch a PG 13 movie alone!")
+  } else if (kids == true) {
+    console.log("You can watch a PG or G movie!")
+  } else (babies == true) {
+    console.log("You are too young to watch most movies!")
+  }
